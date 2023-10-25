c88b0b37395a67e08928ca3f26091e9a52819fcf
Neida <neida@duck.com>
2023-10-25 14:35:24 -0400

    add problem

--- Leetcode/Python/36-valid-sudoku.py
+++ Leetcode/Python/36-valid-sudoku.py
@@ -44,3 +44,54 @@ Constraints:
     board[i][j] is a digit 1-9 or '.'.
 
 '''
+# APPROACH 
+# create dictionaries to store the numbers in the rows, columns, boxes. make the values sets so that repeats are dealt with
+# iterate through each cell in the board (nested loops)
+# skip the empty cells (.)
+# check if the current cell is valid by checking if that number is already in the sets - return False if it is
+# if it is not already there, update sets to include it
+# return True 
+from collections import defaultdict
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = defaultdict(set)
+        columns = defaultdict(set)
+        boxes = defaultdict(set)
+
+        for row in range(9):
+            for col in range(9):
+                current = board[row][col]
+                if current == ".":
+                    continue
+                if (current in rows[row] or current in columns[col] or current in boxes[row//3, col//3]):
+                    return False
+                rows[row].add(current)
+                columns[col].add(current)
+                boxes[row//3,col//3].add(current)
+        return True
+    
+
+# SAMPLE SOLUTION
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        cols = defaultdict(set)
+        rows = defaultdict(set)
+        squares = defaultdict(set)  # key = (r /3, c /3)
+
+        for r in range(9):
+            for c in range(9):
+                if board[r][c] == ".":
+                    continue
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
+                    return False
+                cols[c].add(board[r][c])
+                rows[r].add(board[r][c])
+                squares[(r // 3, c // 3)].add(board[r][c])
+
+        return True
+
+
