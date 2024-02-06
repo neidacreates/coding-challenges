0f4f143aa0056f4ba06fb23ffa834f0b207052b7
Neida <neida@duck.com>
2024-02-06 17:17:51 -0500

    add problems

---
+++ Code Wars/Python/8-kyu-grasshopper-messi.py
@@ -0,0 +1,26 @@
+'''
+Messi is a soccer player with goals in three leagues:
+
+    LaLiga
+    Copa del Rey
+    Champions
+
+Complete the function to return his total number of goals in all three leagues.
+
+Note: the input will always be valid.
+
+For example:
+
+5, 10, 2  -->  17
+
+https://www.codewars.com/kata/55f73be6e12baaa5900000d4/train/python
+'''
+def goals(laLiga, copaDelRey, championsLeague):
+    return laLiga + copaDelRey + championsLeague
+
+# other solutions
+def goals(*args):
+    return sum(args)
+
+def goals(*a):
+    return sum(a)
